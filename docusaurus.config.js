527801f659977ee2181451ce77f052432a03117c
Paul Maier <pauljustus279@gmail.com>
2023-09-15 14:47:30 +0200

    Updated config after setting domain in gh pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Birklehof Docs",
   url: "https://birklehof.github.io/",
-  baseUrl: "/docs",
+  baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.png",
